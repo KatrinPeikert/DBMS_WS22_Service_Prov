14615c1e7eab6417f893fd0f61d8a530d9401d7d
JanFfm <45403124+JanFfm@users.noreply.github.com>
2022-12-28 22:49:15 +0100

    Multipage layout and browserrouting working

---
+++ frontend/src/header.js
@@ -0,0 +1,5 @@
+import * as React from "react";
+import './App.css';
+
+export default function Header() {
+    return (<><h1>Site Title</h1><h2>A  page for rating several services</h2></>)}
---
+++ frontend/src/pages/frontpage.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function FrontPage() {
+return (
+  <>
+  <h1>Hey from FrontPage</h1>
+  <p>This is your awesome FrontPage subtitle</p>
+  </>
+);
+}
